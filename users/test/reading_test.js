4fc9e6e4296b4405115578ea6dfb07b56249d5d8
David Eliason <david@davethemaker.com>
2018-07-05 12:31:03 -0700

    crete script to use nodomon for testing mocha and rendeirng min output

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -15,7 +15,7 @@ describe('Reading users out of mongod db', () => {
             name: "Joe"
         })
             .then((users) => {
-                console.log(users);
+                assert(users[0]._id.toString() === joe._id.toString());
                 done();
             });
     });
