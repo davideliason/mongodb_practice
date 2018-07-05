d7f93a7e80e4c74641b9f31f8494a9ed9b9b30a2
David Eliason <david@davethemaker.com>
2018-07-05 12:12:04 -0700

    successfully retrieves all Joe documents for mocha test

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 const User = require('../src/user');
 
+mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://userstest:userstest10120@ds227821.mlab.com:27821/users_test", { useNewUrlParser: true })
-mongoose.connection
-    .once('open', () => console.log("mongoDB connected"))
-    .on('error', (error) => {
-        console.warn("warning", error);
-    });
+before((done) => {
+    mongoose.connect("mongodb://userstest:userstest10120@ds227821.mlab.com:27821/users_test", { useNewUrlParser: true })
+    mongoose.connection
+        .once('open', () => { done(); })
+        .on('error', (error) => {
+            console.warn("warning", error);
+        });
+});
+
 
 beforeEach((done) => {
     mongoose.connection.collections.users.drop((() => {
