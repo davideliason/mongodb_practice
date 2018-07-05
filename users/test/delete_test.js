ee43cd6bd41beb800c590ad3e930279719f6107b
David Eliason <david@davethemaker.com>
2018-07-05 13:34:06 -0700

    create chained Promises fo delete_test using model instance.remove and class.findOne for checking values

---
+++ users/test/delete_test.js
@@ -0,0 +1,48 @@
+const assert = require('assert');
+const User = require('../src/user'); // model class
+
+describe('Deleting a user', () => {
+    let joe;
+
+    beforeEach((done) => {
+        joe = new User({ name: "Joe" });
+        joe.save()
+            .then(() => done());
+    });
+
+    it('model instance remove', (done) => {
+        joe.remove()
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then((user) => {
+                assert(user === null);
+                done();
+            });
+    });
+
+    it('class method remove', () => {
+
+    });
+    it('class method findAndRemove', () => {
+
+    });
+
+    it('class method findByIdAndRemove', () => {
+
+    });
+});
+
+describe('Creating records', () => {
+    it('saves a user', (done) => {
+        // mdodel instance
+        const joe = new User({
+            name: "Joe"
+        });
+
+        joe.save()
+            .then(() => {
+                // successful save?
+                assert(!joe.isNew);
+                done();
+            })
+    });
+});
