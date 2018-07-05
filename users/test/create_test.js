ac3705e716bbad05ad8ad8845baec42eb19faccf
David Eliason <david@davethemaker.com>
2018-07-05 11:01:30 -0700

    include port on the connection string per the new connection URI option as required

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -2,11 +2,16 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating records', () => {
-    it('saves a user', () => {
+    it('saves a user', (done) => {
         const joe = new User({
-            name: "Jill"
+            name: "Joe"
         });
 
-        joe.save();
+        joe.save()
+            .then(() => {
+                // successful save?
+                assert(!joe.isNew);
+                done();
+            })
     });
 });
--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const User = require('../src/user');
 
 
-mongoose.connect("mongodb://userstest:userstest10120@ds227821.mlab.com:27821/users_test")
+mongoose.connect("mongodb://userstest:userstest10120@ds227821.mlab.com:27821/users_test", { useNewUrlParser: true })
 mongoose.connection
     .once('open', () => console.log("mongoDB connected"))
     .on('error', (error) => {
